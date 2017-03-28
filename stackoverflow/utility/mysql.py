1dbb9b300617abf0bcc19cbdffda5a0a7796015c
chenjiandong <chenjiandongx@qq.com>
2017-03-28 13:24:04 +0800

    v 1.1

--- stackoverflow/utility/mysql.py
+++ stackoverflow/utility/mysql.py
@@ -4,11 +4,22 @@ import pymysql
 conn = pymysql.connect(host="localhost", port=3306, user="root", passwd="0303", db="chenx", charset="utf8")
 cur = conn.cursor()
 
+# Create databse table SQL
+"""
+create table stackoverflow(
+    s_links int(11) not null primary key,
+    s_views int(11),
+    s_votes int(11),
+    s_answers int(11),
+    s_tags text,
+    s_questions text
+);
+
+"""
 with open(r"e:\python\stackoverflow\data\data1.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
     def insert_db(s_links, s_views, s_votes, s_answers, s_tags, s_questions):
-        """" Insert data to database """
 
         sql = "insert into stackoverflow(s_links, s_views, s_votes, s_answers, s_tags, s_questions) " \
               "values(%s, %s, %s, %s, %s, %s)"
