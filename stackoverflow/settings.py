e00e49f1fa3971cb5517eac071a09717c85401e2
chenjiandongx <chenjiandongx@qq.com>
2018-03-11 21:10:37 +0800

    新增日志记录模块

--- stackoverflow/settings.py
+++ stackoverflow/settings.py
@@ -26,7 +26,7 @@ CONCURRENT_ITEMS = 100
 # his setting is also affected by the RANDOMIZE_DOWNLOAD_DELAY setting (which is enabled by default).
 # By default, Scrapy doesn’t wait a fixed amount of time between requests,
 # but uses a random interval between 0.5 * DOWNLOAD_DELAY and 1.5 * DOWNLOAD_DELAY.
-DOWNLOAD_DELAY = 2.5
+DOWNLOAD_DELAY = 4
 
 
 # The download delay setting will honor only one of:
--- stackoverflow/spiders/stackoverflow_spider.py
+++ stackoverflow/spiders/stackoverflow_spider.py
@@ -1,22 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import logging
+
 import scrapy
 from stackoverflow.spiders.items import StackoverflowItem
+
+
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger('monitor')
+logger.setLevel(logging.INFO)
+
+fh = logging.FileHandler('monitor.log')
+fh.setLevel(logging.INFO)
+
+fh.setFormatter(formatter)
+logger.addHandler(fh)
 
 
 class StackoverflowSpider(scrapy.Spider):
 
     name = "stackoverflow"
 
+    def __init__(self):
+        self.count = 1
+
     def start_requests(self):
         _url = 'https://stackoverflow.com/questions?page={page}&sort=votes&pagesize=50'
-        urls = [_url.format(page=page) for page in range(1, 21)]
+        urls = [_url.format(page=page) for page in range(1, 100001)]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         for index in range(1, 51):
+            self.count += 1
+            if self.count % 100 == 0:
+                logger.info(self.count)
+
             sel = response.xpath('//*[@id="questions"]/div[{index}]'.format(index=index))
             item = StackoverflowItem()
             item['votes'] = sel.xpath(
